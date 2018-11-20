7579f113d31af5caac73054aad64ed085cf20459
Sarah Saber <sarahsaber@Sarahs-MacBook-Pro.local>
2018-11-19 23:25:25 -0800

    build markov model and test

--- src/Markov.java
+++ src/Markov.java
@@ -1,2 +1,55 @@
 public class Markov {
+
+    // member variables
+    private int order;                      // order of each n-gram
+    private int length;                     // length of text to be generated
+    private Model model;                    // Markov model after reading text
+    private StringBuilder generatedText;    // generated text using info in model
+
+    // ctor
+    public Markov(int order, int length){
+        this.order = order;
+        this.length = length;
+        this.model = new Model();
+        this.generatedText = new StringBuilder();
+    }
+
+    // build markov chain
+    public void buildChain(String str){
+        this.model.readAndBuild(str, this.order);
+    }
+
+    public String generateText(){
+
+        // Generate a new gram with the specified length
+        for (int i = 0; i < this.length; i++){
+
+        }
+        return this.generatedText.toString();
+    }
+
+    // setters and getters
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+    }
 }
--- src/Model.java
+++ src/Model.java
@@ -1,2 +1,49 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Model {
+
+    // member variables
+    private HashMap<String, ArrayList<Character>> dictionary; // a map of n-grams to list of possible next elements
+
+    // ctor
+    public Model() {
+        dictionary = new HashMap<>();
+    }
+
+    // read text to build model
+    public void readAndBuild(String str, int order){
+        for (int i = 0; i < str.length() - order; i++) {
+            // extract gram
+            String gram = str.substring(i, i + order);
+            // get next character
+            Character nextChar = str.charAt(i + order);
+            // if the gram is not defined in the dictionary
+            if (!dictionary.containsKey(gram)){
+                this.dictionary.put(gram, new ArrayList<Character>());
+            }
+            ArrayList<Character> dummy = this.dictionary.get(gram);
+            dummy.add(nextChar);
+            this.dictionary.put(gram, dummy);
+        }
+    }
+
+    // overrides Object toString method
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+
+        for (String key : dictionary.keySet()){
+            s.append(key + " " + dictionary.get(key).toString() + "\n");
+        }
+
+        return s.toString();
+    }
+
+    // abcdef   order = 3       length = 6
+    // abc
+    // bcd
+    // cde
+    // def
+
+
 }
