2d9adef4eead3312063412c17ca09427dfafb143
sasaber <sarahsaber@Sarahs-MacBook-Pro.local>
2018-11-25 17:21:27 -0800

    add GeneReader

--- src/GeneReader.java
+++ src/GeneReader.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
 
 public class GeneReader {
+
+    // instance variables
+    private BufferedReader br;
+
+    // ctor
+    public GeneReader(BufferedReader br){
+        this.br = br;
+    }
+
+    // read gene from given file and return a new long string
+    public String readGene(){
+
+        return null;
+    }
 }
